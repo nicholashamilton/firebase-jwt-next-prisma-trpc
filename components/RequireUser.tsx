b92b3fdc844403c03a05a748ad00a38b63c0d38b
Nicholas Hamilton <nicholashamilton.co@gmail.com>
2024-07-05 03:27:46 -0400

    add: RequireUser component

---
+++ components/RequireUser.tsx
@@ -0,0 +1,53 @@
+// components/RequireUser.tsx
+import { useUserContext } from "@/context/user/useUserContext";
+import { ReactNode } from "react";
+import LoadingSpinner from "./LoadingSpinner/LoadingSpinner";
+import { useRedirectToLoginIfNoUser } from "@/hooks/user/useRedirectToLoginIfNoUser";
+import { User } from "@firebase/auth";
+
+interface RequireUserProps {
+    children: (user: User) => ReactNode;
+}
+
+export default function RequireUser({ children }: RequireUserProps) {
+    const { user } = useUserContext();
+
+    useRedirectToLoginIfNoUser();
+
+    if (!user) {
+        return (
+            <div className="p-8 w-full flex justify-center">
+                <LoadingSpinner />
+            </div>
+        );
+    }
+
+    return <>{children(user)}</>;
+}
+
+
+
+
+
+
+// import { useUserContext } from "@/context/user/useUserContext";
+// import { PropsWithChildren } from "react";
+// import LoadingSpinner from "./LoadingSpinner/LoadingSpinner";
+// import { useRedirectToLoginIfNoUser } from "@/hooks/user/useRedirectToLoginIfNoUser";
+
+// export default function RequireUser(props: PropsWithChildren) {
+
+//     const { user } = useUserContext();
+
+//     useRedirectToLoginIfNoUser();
+
+//     if (!user) {
+//         return (
+//             <div className="p-8 w-full flex justify-center">
+//                 <LoadingSpinner />
+//             </div>
+//         );
+//     }
+
+//     return props.children;
+// }
